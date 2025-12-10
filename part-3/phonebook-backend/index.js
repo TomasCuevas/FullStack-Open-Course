a68157786bde907d4a97d82c0eaef5ee492b6265
Tomás Cuevas <tomas.contact.dev@gmail.com>
2025-12-10 17:23:19 -0300

    uibuild

---
+++ part-3/phonebook-backend/index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const morgan = require("morgan");
+let { persons } = require("./persons.js");
+
+const app = express();
+morgan.token("body", (request) => JSON.stringify(request.body));
+
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons).status(200);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.post("/api/persons", (request, response) => {
+  const { name, number } = request.body;
+
+  if (!name || !number) {
+    return response.status(400).json({
+      error: "Name or number is missing",
+    });
+  }
+
+  const personExists = persons.find((person) => person.name === name);
+
+  if (personExists) {
+    return response.status(400).json({
+      error: "Name must be unique",
+    });
+  }
+
+  const person = {
+    id: Math.round(Math.random() * 1000000),
+    name,
+    number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.get("/info", (request, response) => {
+  const date = new Date();
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
+  );
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
